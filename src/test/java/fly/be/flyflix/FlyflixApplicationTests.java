0072bffeb4eed5b791113eb3055f88c4d00cdd8c
Paula Gimenez <paulaherbal@gmail.com>
2023-07-01 16:40:11 -0300

    First commit

---
+++ src/test/java/fly/be/flyflix/FlyflixApplicationTests.java
@@ -0,0 +1,13 @@
+package fly.be.flyflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
