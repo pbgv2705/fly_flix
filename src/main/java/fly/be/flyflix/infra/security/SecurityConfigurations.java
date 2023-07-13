b680a6a8ef16a5f29e3e617a974f6c37a310c813
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-07-13 17:44:08 -0300

    Criando as configurações de segurança e adicionando os métodos de filtrar e autenticar

--- src/main/java/fly/be/flyflix/infra/security/SecurityConfigurations.java
+++ src/main/java/fly/be/flyflix/infra/security/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package fly.be.flyflix.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,4 +20,8 @@ public class SecurityConfigurations {
                 .and().build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
 }
