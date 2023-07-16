bfdb7e8c653c86d98634175e2bd8b455ae3a41ce
Camila Oliveira <camilamoliveiratech@gmail.com>
2023-07-16 17:01:45 -0300

    Adicionando novo método que será utilizado pelo filtro, SecurityFilter.

--- src/main/java/fly/be/flyflix/infra/security/TokenService.java
+++ src/main/java/fly/be/flyflix/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package fly.be.flyflix.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import fly.be.flyflix.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -31,5 +32,18 @@ public class TokenService {
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API FlyFlix")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
     }
 }
