883d83b9aa0535f52c4af451c1604027fde452ac
ldealejandro <letzalet@gmail.com>
2025-04-13 15:57:59 -0300

    adicionando categorias de cursos, pequenos ajustes para usuario admin para melhores testes

--- src/main/java/fly/be/flyflix/auth/entity/PerfilAluno.java
+++ src/main/java/fly/be/flyflix/auth/entity/PerfilAluno.java
@@ -2,8 +2,13 @@ package fly.be.flyflix.auth.entity;
 
 public enum PerfilAluno {
 
+    ALL,
     COLORINDO,
-    MULHERES_IN_TECH
+    MULHERES_IN_TECH,
+    MERCADO,
+    TECNOLOGIA,
+    MARKETING_E_EMPREENDEDORISMO,
+    SUPERPROFS
 
 
 }
--- src/main/java/fly/be/flyflix/config/DefaultAdminUserConfig.java
+++ src/main/java/fly/be/flyflix/config/DefaultAdminUserConfig.java
@@ -1,8 +1,11 @@
 package fly.be.flyflix.config;
 
 
+import fly.be.flyflix.auth.entity.Aluno;
+import fly.be.flyflix.auth.entity.PerfilAluno;
 import fly.be.flyflix.auth.entity.PerfilUsuario;
 import fly.be.flyflix.auth.entity.Usuario;
+import fly.be.flyflix.auth.repository.AlunoRepository;
 import fly.be.flyflix.auth.repository.PerfilUsuarioRepository;
 import fly.be.flyflix.auth.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Configuration
@@ -18,6 +22,8 @@ public class DefaultAdminUserConfig implements CommandLineRunner {
 
     @Autowired
     private PerfilUsuarioRepository perfilUsuarioRepository;
+    @Autowired
+    private AlunoRepository alunoRepository;
     @Autowired
     private UsuarioRepository usuarioRepository;
     @Autowired
@@ -33,30 +39,55 @@ public class DefaultAdminUserConfig implements CommandLineRunner {
         System.out.println("******************************************************************************************" );
         System.out.println("RoleAdmin: " + roleAdmin);
         System.out.println("******************************************************************************************" );
-        var userAdmin = usuarioRepository.findByLogin("admin@admin.com");
+        final String adminEmail = "admin@admin.com";
+        final String adminCpf = "68669859432";
+        var userAdmin = usuarioRepository.findByLogin(adminEmail);
+        var alunoAdmin = alunoRepository.findByEmail(adminEmail);
 
         if (roleAdmin == null) {
-            System.out.println("Role ADMIN not found in the database. Aborting user creation.");
+            System.out.println("Role ADMIN not found in the database. ");
             return;
         }
-
         userAdmin.ifPresentOrElse(
                 // User admin already exists
                 user ->{
 
-                    System.out.println("Creating admin user");
+                    System.out.println("Admin already exist");
 
                 },
                 () -> { // User admin not exists and will be created
                     var usuario = new Usuario();
-                    usuario.setLogin("admin@admin.com");
-                    usuario.setCpf("68669859432");
-                    usuario.setSenha(passwordEncoder.encode("FlyAdmin"));
+                    usuario.setLogin(adminEmail);
+                    usuario.setCpf(adminCpf);
+                    usuario.setSenha(passwordEncoder.encode("FlyAdmin")); // SENHA ADMINISTRADOR
                     usuario.setPerfiles(Set.of(roleAdmin));
                     usuarioRepository.save(usuario);
 
                     System.out.println("User admin created");
                     System.out.println(usuario);
+
+
+                });
+
+        alunoAdmin.ifPresentOrElse(
+                // User Aluno admin already exists
+                user ->{
+
+                    System.out.println("Aluno admin already exist");
+
+                },
+                () -> { // User admin aluno not exists and will be created
+                    var aluno = new Aluno();
+                    aluno.setNome("Admin");
+                    aluno.setEmail(adminEmail);
+                    aluno.setCpf(adminCpf);
+                    aluno.setPerfilAluno(PerfilAluno.valueOf("ALL"));
+                    aluno.setAtivo(true);
+                    aluno.setDataNascimento(LocalDate.of(2022, 1, 17));
+                    alunoRepository.save(aluno);
+
+                    System.out.println("User admin Aluno created");
+                    System.out.println(aluno);
 
 
                 });
